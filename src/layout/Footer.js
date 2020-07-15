75cab71b6bf9581972d6df25f7dc31a0fb3665a4
Hrishrath <rish.rathore9@gmail.com>
2020-07-16 01:39:08 +0530

    Project Upload

---
+++ src/layout/Footer.js
@@ -0,0 +1,12 @@
+//TODO DONE: Export the Footer
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className=" text-center pt-3 pb-2 mt-5 sticky-bottom">
+      <span className="text-white">A simple Contact App</span>
+    </footer>
+  );
+};
+
+export default Footer
---
+++ src/utils/config.js
@@ -0,0 +1,21 @@
+//TODO DONE: add firebase configuration and export it
+
+export const firebaseConfig = {
+  apiKey: "#################",
+  authDomain: "########################",
+  databaseURL: "#######################",
+  projectId: "####################",
+  storageBucket: "#######################",
+  messagingSenderId: "#####################",
+  appId: "###############################3"
+};
+
+
+//image configuration
+export const imageConfig = {
+  quality: 0.2,
+  maxWidth: 800,
+  maxHeight: 600,
+  autoRotate: true
+};
+
